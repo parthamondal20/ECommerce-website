658b2bf2b81fc23f7bc3451d6e6c260f6dda48b3
Parthamondal <parthamondal03620@gmail.com>
2025-08-29 15:20:55 +0530

    Fixed  multer.middleware.js

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -2,10 +2,10 @@ import multer from "multer";
 const storage = multer.diskStorage({
   // it will have the access of the temp file
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "../../public/temp");
   },
   filename: function (req, file, cb) {
-    const filePath = `./public/temp/${file.originalname}`;
+    const filePath = `../../public/temp/${file.originalname}`;
     console.log(filePath);
     cb(null, file.originalname);
   },
